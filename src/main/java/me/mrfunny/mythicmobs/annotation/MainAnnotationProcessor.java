fe9dc5700e184f5cc3d62d6ee99bab78fbaaf48c
Sasha Semenishchev <sashasemenishchev@gmail.com>
2022-08-22 21:38:20 +0200

    A lot of job done

--- src/main/java/me/mrfunny/mythicmobs/annotation/MainAnnotationProcessor.java
+++ src/main/java/me/mrfunny/mythicmobs/annotation/MainAnnotationProcessor.java
@@ -1,6 +1,7 @@
 package me.mrfunny.mythicmobs.annotation;
 
 import com.google.auto.service.AutoService;
+import me.mrfunny.mythicmobs.wrapper.Mob;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
@@ -8,6 +9,9 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
@@ -17,7 +21,7 @@ import java.util.Map;
 import java.util.Set;
 
 @AutoService(Processor.class)
-@SupportedSourceVersion(SourceVersion.RELEASE_17)
+@SupportedSourceVersion(SourceVersion.RELEASE_16)
 @SupportedAnnotationTypes("me.mrfunny.mythicmobs.annotation.*")
 public class MainAnnotationProcessor extends AbstractProcessor {
     private boolean created = false;
@@ -25,45 +29,60 @@ public class MainAnnotationProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         String packageName = "";
         Map<String, String[]> generated = new HashMap<>();
-        for(Element rawElement : roundEnvironment.getElementsAnnotatedWith(Signal.class)) {
-            if(rawElement.getKind() != ElementKind.CLASS) {
+        boolean empty = true;
+        for(Element element : roundEnvironment.getElementsAnnotatedWith(Signal.class)) {
+            if(element.getKind() != ElementKind.CLASS) {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "@Signals annotation is only supported by classes");
                 continue;
             }
-            String className = rawElement.asType().toString();
-            print("Generating signals for " + className);
-            PackageElement packageE = processingEnv.getElementUtils().getPackageOf(rawElement);
+            TypeMirror type = element.asType();
+            if(!isInstance(type, Mob.class)) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "Class " + type + " is annotated with @Signal it doesn't implement Mob class (" + Mob.class.getName() + ")");
+            }
+            empty = false;
+            String className = type.toString();
+            int lastDot = className.lastIndexOf('.');
+            String simpleClassName = className.substring(lastDot + 1);
+            PackageElement packageE = processingEnv.getElementUtils().getPackageOf(element);
             packageName = packageE.getQualifiedName().toString();
             if(processingEnv.getElementUtils().getAllTypeElements(packageName).stream().anyMatch(e ->
                 e.getSimpleName().contentEquals(className)
             )) {
                 packageName += ".signals";
             }
-            String[] generate = rawElement.getAnnotation(Signal.class).value();
+            String[] generate = element.getAnnotation(Signal.class).value();
             for (String s : generate) {
                 if(s.trim().equals("")) {
                     processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "@Signal values can't accept empty values (\"\")");
                 }
             }
-            generated.put(rawElement.asType().toString(), generate);
+            generated.put(simpleClassName, generate);
         }
 
-        if(created) {
+        if(created && empty) {
             return true;
         }
 
         try {
-
-            JavaFileObject enumFile = processingEnv.getFiler().createSourceFile(packageName + "Signals");
+            String toCreate;
+            boolean packageNameEmpty = packageName.trim().equals("");
+            if(packageNameEmpty) {
+                toCreate = "Signals";
+            } else {
+                toCreate = packageName + ".Signals";
+            }
+            print("Choosing " + toCreate + " as Signals class name");
+            JavaFileObject enumFile = processingEnv.getFiler().createSourceFile(toCreate);
             try(PrintWriter out = new PrintWriter(enumFile.openWriter())) {
-                if(!packageName.trim().equals("")) {
+                if(!packageNameEmpty) {
                     out.println("package " + packageName + ";");
                 }
+                out.println();
                 out.println("public class Signals {");
                 for(Map.Entry<String, String[]> entry : generated.entrySet()) {
-                    out.println("public static enum For" + entry.getKey() + " implements me.mrfunny.mythicmobs.internal.MobSignal {");
-                    out.println(String.join(",", entry.getValue()));
-                    out.println("}");
+                    out.println("    public static enum For" + entry.getKey() + " implements me.mrfunny.mythicmobs.internal.MobSignal {");
+                    out.println("        " + String.join(",", entry.getValue()));
+                    out.println("    }");
                 }
 
                 out.println("}");
@@ -78,5 +97,13 @@ public class MainAnnotationProcessor extends AbstractProcessor {
 
     private void print(String str) {
         processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, str);
+    }
+
+    private boolean isInstance(TypeMirror type, Class<?> clazz) {
+        Types types = processingEnv.getTypeUtils();
+        Elements elements = processingEnv.getElementUtils();
+        TypeMirror tm = elements.getTypeElement(type.toString()).asType();
+        TypeMirror comparable = types.erasure(elements.getTypeElement(clazz.getName()).asType());
+        return types.isAssignable(tm, comparable);
     }
 }
--- src/main/java/me/mrfunny/mythicmobs/wrapper/Mobs.java
+++ src/main/java/me/mrfunny/mythicmobs/wrapper/Mobs.java
@@ -1,9 +1,17 @@
 package me.mrfunny.mythicmobs.wrapper;
 
+import io.lumine.mythic.bukkit.MythicBukkit;
 import io.lumine.mythic.core.mobs.ActiveMob;
 import me.mrfunny.mythicmobs.internal.MobSignal;
 
 public class Mobs {
+
+    public static MythicBukkit MYTHIC_MOBS = null;
+
+    public static void init() {
+        MYTHIC_MOBS = MythicBukkit.inst();
+    }
+
     public static void sendSignal(ActiveMob mob, MobSignal signal) {
         mob.signalMob(mob.getEntity(), signal.toString());
     }
